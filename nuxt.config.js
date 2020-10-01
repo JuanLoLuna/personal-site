e164911435e18d92cb10ca333921e3057f42fad2
JuanLoLuna <juanlo.luna@gmail.com>
2020-10-01 11:14:13 -0500

    basic setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,48 +1,51 @@
 export default {
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'personal-site',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+    head: {
+        title: 'Juan F. Luna',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    css: [
+        '~/assets/css/main.scss'
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
-
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
-
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build'
-  ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/bootstrap
-    '@nuxtjs/bulma'
-  ],
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-    postcss: {
-      preset: {
-        features: {
-          customProperties: false
+    plugins: [
+    ],
+    components: true,
+    buildModules: [
+        '@nuxt/typescript-build'
+    ],
+    modules: [
+        ['nuxt-buefy', {
+            defaultIconPack: 'fas',
+            defaultIconComponent: 'fa'
+        }],
+        ['nuxt-fontawesome', {
+            component: 'fa',
+            imports: [
+                {
+                    set: '@fortawesome/free-solid-svg-icons',
+                    icons: ['fas']
+                }
+            ]
+        }]
+    ],
+    build: {
+        /*
+        ** You can extend webpack config here
+        */
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        extend (config, ctx) {
+            '@nuxtjs/eslint-config-typescript'
+        },
+        hotMiddleware: {
+            client: {
+                overlay: false
+            }
         }
-      }
     }
-  }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,35 @@
 <template>
     <div>
-        <h1 class="subtitle is-marginless">
-            Personal
-        </h1>
-        <hr class="is-marginless">
+        <section class="hero is-dark is-large">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title is-size-2">
+                        Juan Francisco López Luna
+                    </h1>
+                    <h2 class="subtitle is-family-secondary">
+                        MSc Electrical Engineering
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="mt-large container has-text-right">
+                <h1 class="title is-size-2">
+                    Who am I?
+                </h1>
+                <p class="is-family-secondary">
+                    A MSc in bioelectronics with a strong scientific background and significant entrepreneur experience. Founded a tech company and developed end-to-end projects implementing innovative technologies into both new and old processes exceeding clients expectations. Associate professor of biomedical instrumentation and mentor of research projects in an academic institution. Passionate about research and technological innovation.
+                </p>
+            </div>
+            <div class="mt-large container">
+                <h1 class="title is-size-2">
+                    What have I done?
+                </h1>
+                <p class="is-family-secondary">
+                    A MSc in bioelectronics with a strong scientific background and significant entrepreneur experience. Founded a tech company and developed end-to-end projects implementing innovative technologies into both new and old processes exceeding clients expectations. Associate professor of biomedical instrumentation and mentor of research projects in an academic institution. Passionate about research and technological innovation.
+                </p>
+            </div>
+        </section>
     </div>
 </template>
 
